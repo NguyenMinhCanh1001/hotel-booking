7b2c8a0d73c10eee9c34e53889a28538c3b0fef7
phamtuan7592 <phamtuan7592@gmail.com>
2024-10-08 20:59:23 +0700

    index

--- index.php
+++ index.php
@@ -225,6 +225,70 @@
     </div>
   </div>
 
+  <!-- Phòng -->
+  <h2 class="mt-5 pt-4 mb-4 text-center merienda-header">Phòng</h2>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4 col-md-6 my-3">
+      <div class="card border-0 shadow" style="max-width: 350px; margin: auto;  ">
+        <img src="/images/Phong/1.jpg" class="card-img-top">
+          <div class="card-body">
+            <h5 >Phòng bình dân</h5>
+            <h6 class="mb-3">300.000 VND/đêm</h6>
+            <div class="features mb-4">
+              <h6 class="mb-1">Điểm đặc trưng
+              </h6>
+              <span class="badge rounded-pill bg-light text-dark text-wrap">
+                2 phòng ngủ  
+              </span>
+              <span class="badge rounded-pill bg-light text-dark text-wrap">
+                1 ban công
+              </span>
+              <span class="badge rounded-pill bg-light text-dark text-wrap">
+                2 phòng tắm  
+              </span>
+              <span class="badge rounded-pill bg-light text-dark text-wrap">
+                2 ghế tình yêu
+              </span>
+            </div>
+            <div class="facilities mb-4">
+            <h6 class="mb-1">Tiện nghi</h6>
+            <span class="badge rounded-pill bg-light text-dark text-wrap">
+                Wife 
+              </span>
+              <span class="badge rounded-pill bg-light text-dark text-wrap">
+                Tivi
+              </span>
+              <span class="badge rounded-pill bg-light text-dark text-wrap">
+                Điều hòa
+              </span>
+              <span class="badge rounded-pill bg-light text-dark text-wrap">
+                Cách âm
+              </span>
+              </div>
+              <div class="rating mb-4">
+              <h6 class="mb-1">Đánh giá</h6>
+              <span class="badge rounded-pill bg-light">
+                <i class="bi bi-star-fill text-warning"></i>
+                <i class="bi bi-star-fill text-warning"></i>
+                <i class="bi bi-star-fill text-warning"></i>
+                <i class="bi bi-star-half text-warning"></i>
+            </span>
+              </div>
+              <div class="d-flex justify-content-evenly mb-2">
+                <a href="#" class="btn btn-sm text-white custom-bg shadow-none">Đặt phòng</a>
+                <a href="#" class="btn btn-sm btn btn-outline-primary shadow-none ">Xem chi tiết</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-12 text-center mt-5">
+        <a href="#" class="btn btn-sm btn-outline-dark rounded-0 poppins-medium ">Thêm Phòng</a>
+      </div>
+    </div>
+  </div>
+
 <br><br><br>
 <br><br><br>
 
