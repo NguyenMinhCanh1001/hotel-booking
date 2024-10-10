7cd1b5233d8410eebbda214fcdf55ecc7da2762e
phamtuan7592 <phamtuan7592@gmail.com>
2024-10-10 17:54:09 +0700

    index

---
+++ inc/footer.php
@@ -0,0 +1,36 @@
+<div class="container-fluid bg-white mt-5">
+  <div class="row">
+    <div class="col-lg-4 p-4">
+      <h3 class="merienda-header fs-3 mb-2"> UTH HOTEL</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis, excepturi laboriosam? Nulla ex, corporis quis dolores esse, totam nisi cum iusto odio aspernatur ipsa eos ducimus corrupti quod, quos suscipit.
+      </p>
+    </div>
+    <div class="col-lg-4 p-4">
+      <h5 class="mb-3">Links</h5>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Trang chủ</a><br>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Phòng</a><br>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Cơ sở vật chất</a><br>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">liên hệ</a><br>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Giới thiệu</a><br>
+      </div>
+      <div class="col-lg-4 p-4">
+        <h5 class="mb-3">Theo dõi chúng tôi trên</h5>
+        <a href="#" class="d-inline-block text-dark text-decoration-none mb-3 ">
+              <i class="bi bi-facebook me-l"></i> Facebook
+          </a>
+          <br>
+            <a href="#" class="d-inline-block text-dark text-decoration-none mb-3 ">
+              <i class="bi bi-tiktok"></i> TikTok
+          </a>
+          <br>
+            <a href="#" class="d-inline-block text-dark text-decoration-none mb-2 ">
+              <i class="bi bi-instagram"></i> Instagram
+          </a>
+      </div>
+  </div>
+</div>
+
+
+
+<h6 class="text-center bg-dark text-white p-3 m-0">NHÓM 3</h6>
