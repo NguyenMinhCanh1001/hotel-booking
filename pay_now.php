8e44eee656195a279c7524253230d93fc51615a3
phamtuan7592 <phamtuan7592@gmail.com>
2024-11-12 22:54:51 +0700

    in

--- pay_now.php
+++ pay_now.php
@@ -100,26 +100,27 @@ $returnData = array('code' => '00'
         , 'message' => 'success'
         , 'data' => $vnp_Url);
         if (isset($_POST['pay_now'])) {
+            $frm_data = filteration($_POST);
+
+            $query1 = "INSERT INTO booking_order (user_id, room_id, check_in, check_out, order_id) 
+                    VALUES (?, ?, ?, ?, ?)";
+
+            insert($query1, [$CUST_ID, $_SESSION['room']['id'],$frm_data['Checkin'], $frm_data['Checkout'], $vnp_TxnRef],'issss');
+
+            $booking_id = mysqli_insert_id($con);
+
+            $query2 = "INSERT INTO booking_details ( booking_id, room_name, price, total_pay, user_name, phonenum, address) 
+                    VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+            insert($query2,[$booking_id, $_SESSION['room']['name'], $_SESSION['room']['price'], $vnp_Amount, $frm_data['name'],$frm_data['phonenum'],$frm_data['address']],'issssss');
+        
             header('Location: ' . $vnp_Url);
         } else {
             echo json_encode($returnData);
         }
 
 
-        if (isset($_POST['pay_now'])) {
-        $frm_data = filteration($_POST);
-
-        $query1 = "INSERT INTO booking_order (user_id, room_id, check_in, check_out, order_id) 
-                VALUES (?, ?, ?, ?, ?)";
-
-        insert($query1, [$CUST_ID, $_SESSION['room']['id'],$frm_data['Checkin'], $frm_data['Checkout'], $vnp_TxnRef],'issss');
-
-        $booking_id = mysqli_insert_id($con);
-
-        $query2 = "INSERT INTO booking_details ( booking_id, room_name, price, total_pay, user_name, phonenum, address) 
-                VALUES (?, ?, ?, ?, ?, ?, ?)";
-
-        insert($query2,[$booking_id, $_SESSION['room']['name'], $_SESSION['room']['price'], $vnp_Amount, $frm_data['name'],$frm_data['phonenum'],$frm_data['address']],'issssss');
-        }
+        
+        
 
 ?>
